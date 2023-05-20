03bf11abc52d19cb3d00bdd0e34c614334610d37
Pandeyviii <vishalkumarpandey665@gmail.com>
2023-05-20 15:54:07 +0530

    creating a server

---
+++ app.js
@@ -0,0 +1,5 @@
+const http=require("http");
+const server=http.createServer((req,res)=>{
+    console.log("vishal pandey");
+})
+server.listen(4000);
---
+++ first-app.js
@@ -0,0 +1,3 @@
+console.log("hello node.js");
+const fs=require("fs");
+fs.writeFileSync("message.txt","hello message! how are u");
