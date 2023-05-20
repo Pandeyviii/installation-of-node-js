0bd9dba1913d414e9408b05f76aba7c523fda34d
Pandeyviii <vishalkumarpandey665@gmail.com>
2023-05-20 16:41:10 +0530

    Request/Response

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const http=require("http");
 const server=http.createServer((req,res)=>{
-    console.log("vishal pandey");
+    //console.log("vishal pandey");
+    if(req.url==='/home'){
+        res.setHeader('Content-type','text-html');
+        res.write('<html><body>Welcome Home</body></html>');
+        //res.writable("Hi! welcome back vishal pandey")
+    }
+    else if(req.url==='/about'){
+        res.write('<html><body>Welcome to about us page</body></html>');
+    }
+   else if(req.url==='/node'){
+    res.write('<html><body>Welcome to node js project</body></html>');
+   }
 })
 server.listen(4000);
