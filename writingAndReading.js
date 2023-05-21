8e2711d30ebc84d9a50f0aca158a9ee29aa3011e
Pandeyviii <vishalkumarpandey665@gmail.com>
2023-05-21 16:39:23 +0530

    express js startimg

--- writingAndReading.js
+++ writingAndReading.js
@@ -1,5 +1,16 @@
-const http=require("http");
-const routes=require("./routes")
-console.log(routes.text);
-const server=http.createServer(routes.handler);
-server.listen(4000);
+//const http=require("http");
+// const routes=require("./routes")
+// console.log(routes.text);
+const express=require("express");
+const app=express();
+app.use((req,res,next)=>{
+    console.log("in the middleware");
+    next();
+})
+app.use((req,res,next)=>{
+    res.send('<h1>Hi vishal pandey welcomes you');
+    console.log("in the second middleware");
+})
+// const server=http.createServer(app);
+// server.listen(4000);
+app.listen(4000);
